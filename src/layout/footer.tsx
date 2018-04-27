206f0630f5cf4379cd0ded14f2e109035b2f6a8c
siren1313 <moriluxuria@gmail.com>
2018-04-28 02:13:00 +0300

    commit

---
+++ src/layout/footer.tsx
@@ -0,0 +1,25 @@
+
+import * as React from 'react';
+import witchPage from '../pages/witchpage';
+import homepage from '../pages/homepage';
+import soonpage from '../pages/soonpage';
+import missionpage from '../pages/missionpage';
+import docsPage from '../pages/docsPage';
+import settingsPage from '../pages/settingsPage';
+import chatPage from '../pages/chatPage';
+import createCharPage from '../pages/createCharPage';
+import {Switch, Route, Redirect} from 'react-router-dom';
+
+
+export default class Main extends React.Component {
+   public render() {
+      return (
+      <footer className="footer">
+         <a href="">
+            <span>heart</span>
+            <i class="fas fa-heart"></i>
+         </a>
+      </footer>
+      );
+   }
+}
